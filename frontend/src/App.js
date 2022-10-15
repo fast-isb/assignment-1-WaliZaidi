0f56b0cab9feb094bb830a97449e7eb1301ec167
Wali Hassan <i202429@nu.edu.pk>
2022-10-16 00:30:57 +0500

    added gradient and app bar with BBMS in white, icon still black idky

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import { ButtonGroup, FormControlLabel } from '@material-ui/core';
+import Checkbox from '@material-ui/core/Checkbox';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles, ThemeProvider, createTheme } from '@material-ui/core/styles'; //import for styles and themes
+import React from 'react';
+import { blue, orange, purple } from '@material-ui/core/colors';
+//import 'fontsource-roboto'; //importing fonts, this is a google font, so we need to import it from google
+import Typography from '@material-ui/core/Typography'; //this is required for the better moving and typography of the page
+import Container from '@material-ui/core/Container'; //containers for MUI
+import Paper from '@material-ui/core/Paper'; //helps in seeing the grid for MUI a bit better
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar'; //this is the app bar, which is the bar at the top of the page
+import Toolbar from '@material-ui/core/Toolbar'; //this for the tool bar
+import IconButton from '@material-ui/core/IconButton'; //this for a button with an icon on it
+import MenuIcon from '@material-ui/icons/Menu'; //and this is the actual icon that we need
+
+const theme = createTheme({ //this is how you create a theme, and then you can use it within the theme provider
+  palette: { //is how colors are changed
+    primary: { //the changing of the primary color in this 
+      main: '#880e4f', //and then the values of the color that you want to change, with the weight of the color in the braces
+    },
+    secondary: {
+      main:blue[300], //secondary
+    }
+  },
+  typography: {
+    h2: {
+      fontSize: 36,
+      marginBottom: 15, //this is how you change the typography of the page, and then you can call it within the typography component
+    }
+  }
+})
 
 function App() {
   return (
+    <ThemeProvider theme={theme}> //this is how you use the theme provider
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AppBar color="primary" style={{padding: '5px'}}>
+          <Toolbar>
+            <IconButton color='white'>
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" style={{fontFamily: '', color: 'white', fontSize: 26}}>
+              BLOOD BANK MANAGEMENT SYSTEM
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </header>
     </div>
+    </ThemeProvider>
   );
 }
 
