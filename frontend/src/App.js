7f6360e415459c1ff10f528aa2787ca2bed59e03
Wali Hassan <i202429@nu.edu.pk>
2022-11-15 05:46:07 +0500

    branchCheckCommit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,12 +14,12 @@ import UpdatePersonalInfo from './updatePersonalInfo';
 
 function App() {
   return (
-    <div className="App">
+    <div data-testid="comp" className="App">
       {/* <Route exact path='/' component={decisionPage}/> */}
       <Routes>
         <Route exact path='/' element={<DecisionPage/>}/> {/*Use the element tag, the component one expired, and we also have to call the thing we passed as a component*/}
         <Route exact path='/signup' element={<SignUp/>}></Route>
-        <Route exact path='/signin' element={<SignInPage/>}></Route>
+        {/* <Route exact path='/signin' element={<SignInPage/>}></Route> */}
         <Route exact path='/main' element={<SubmissionPage/>}></Route>
         <Route exact path='/userUpdate' element={<UpdatePersonalInfo/>}></Route>
       </Routes>
